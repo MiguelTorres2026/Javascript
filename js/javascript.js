f2c9ae77d20bf5b9648d1cff3dd96c6aa5e7a7e4
MiguelTorres2026 <145124057+MiguelTorres2026@users.noreply.github.com>
2024-10-02 12:50:58 -0400

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,35 @@
+console.log("this is external");
+console.log("Work on your typing skills!");
+
+//variable
+let variableName;
+const myName = 'Miguel';
+
+let lukeAge = 16;
+let tylerAge = 17;
+let kevinAge =18;
+let mcdonaldAge = 33;
+
+
+
+
+let old=lukeAge +mcdonaldAge;
+console.log("old age =" + old);
+console.log("old");
+console.log(old+kevinAge)
+//old=old+kevinAge;
+old+= kevinAge;
+console.log(old)
+
+
+
+
+//Arithmetic
+console.log(10 % 2);
+console.log(14 % 2);
+console.log(35 % 2);
+
+let lastName = "McDonald";
+
+lastName=lastName+", Ashley";
+console.log(lastName.length);
